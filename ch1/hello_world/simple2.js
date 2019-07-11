cc2cbc0e496713864c8d50d6b1fb58e41235c2e8
jw <jw6lee@gmail.com>
2019-07-11 00:30:31 -0400

    ch 1

---
+++ ch1/hello_world/simple2.js
@@ -0,0 +1,23 @@
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    const text = this.state.liked ? "좋아요 취소" : "좋아요";
+    return React.createElement(
+      "button",
+      {
+        onClick: () => this.setState({ liked: !this.state.liked })
+      },
+      text
+    );
+  }
+}
+const domContainer1 = document.querySelector("#react-root1");
+const domContainer2 = document.querySelector("#react-root2");
+const domContainer3 = document.querySelector("#react-root3");
+ReactDOM.render(React.createElement(LikeButton), domContainer1);
+ReactDOM.render(React.createElement(LikeButton), domContainer2);
+ReactDOM.render(React.createElement(LikeButton), domContainer3);
