55c8f7c9d951de91ab7e6b092a7debc69f0006c8
jw <jw6lee@gmail.com>
2019-07-15 22:54:40 -0400

    added images

--- 1_chapter/cra-test/src/App.js
+++ 1_chapter/cra-test/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import smallImage from "./small.jpeg";
+import bigImage from "./big.jpeg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={smallImage} alt="small" />
+        <img src={bigImage} alt="big" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to react. Time to Study!</p>
         <a
